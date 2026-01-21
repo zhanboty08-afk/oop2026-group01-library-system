56f2a868293e50cbfcb45f07200826973c5208d9
Zhanbolat Tulepbergen <zhanboty08@gmail.com>
2026-01-21 22:50:46 +0500

    Tulepbergen Zhanbolat- add user

--- untitled/src/main/java/org/example/app/entities/User.java
+++ untitled/src/main/java/org/example/app/entities/User.java
@@ -2,25 +2,25 @@ package org.example.app.entities;
 
 public class User {
     private Long id;
-    private String nameofuser;
+    private String name;
     private String email;
 
     public User() {}
 
-    public User(Long id, String nameofuser, String email) {
-        this.id = id; this.nameofuser = nameofuser; this.email = email;
+    public User(Long id, String name, String email) {
+        this.id = id; this.name = name; this.email = email;
     }
 
-    public User(String nameofuser, String email) {
-        this.nameofuser = nameofuser; this.email = email;
+    public User(String name, String email) {
+        this.name = name; this.email = email;
     }
 
     public Long getId() { return id; }
-    public String getName() { return nameofuser; }
+    public String getName() { return name; }
     public String getEmail() { return email; }
 
     public void setId(Long id) { this.id = id; }
-    public void setName(String nameofuser) { this.nameofuser = nameofuser; }
+    public void setName(String name) { this.name = name; }
     public void setEmail(String email) { this.email = email; }
 }
 
