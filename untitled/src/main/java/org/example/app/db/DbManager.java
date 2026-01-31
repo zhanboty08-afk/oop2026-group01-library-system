067debb2359f843d1531625a63c72ad5119fc6ec
Zhanbolat Tulepbergen <zhanboty08@gmail.com>
2026-01-31 21:20:28 +0500

    Tulepbergen Zhanbolat- add db manager

---
+++ untitled/src/main/java/org/example/app/db/DbManager.java
@@ -0,0 +1,21 @@
+package org.example.app.db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public final class DbManager {
+    private static final DbManager INSTANCE = new DbManager();
+    private final IDatabase database = new PostgresDatabase();
+
+    private DbManager() {
+    }
+
+    public static DbManager getInstance() {
+        return INSTANCE;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return this.database.getConnection();
+    }
+}
+
