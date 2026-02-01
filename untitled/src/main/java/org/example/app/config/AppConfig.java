990f416db4d644e6beab030b174102ef4e5b07ea
Zhanbolat Tulepbergen <zhanboty08@gmail.com>
2026-02-01 10:34:06 +0500

    Tulepbergen Zhanbolat- add appconfig

--- untitled/src/main/java/org/example/app/config/AppConfig.java
+++ untitled/src/main/java/org/example/app/config/AppConfig.java
@@ -1,9 +1,40 @@
 package org.example.app.config;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public final class AppConfig {
+
     private AppConfig() {}
 
-    public static final String DB_URL = "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres";
-    public static final String DB_USER = "postgres.nvimuolinrhwdpmxfviq";
-    public static final String DB_PASS = "C%p$vi.W2?fzURC";
+    public static final String DB_URL =
+            "jdbc:postgresql://aws-1-ap-south-1.pooler.supabase.com:5432/postgres";
+
+    public static final String DB_USER =
+            "postgres.nvimuolinrhwdpmxfviq";
+
+    public static final String DB_PASS = loadPassword();
+
+    private static String loadPassword() {
+        Properties props = new Properties();
+
+        try (InputStream in = AppConfig.class.getClassLoader().getResourceAsStream("config.properties")) {
+
+            if (in == null) {
+                throw new RuntimeException("config.properties not found in src/main/resources");
+            }
+
+            props.load(in);
+
+            String pwd = props.getProperty("DB_PASSWORD");
+            if (pwd == null || pwd.isBlank()) {
+                throw new RuntimeException("DB_PASSWORD not found in config.properties");
+            }
+            return pwd;
+
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot load DB password", e);
+        }
+    }
 }
+
